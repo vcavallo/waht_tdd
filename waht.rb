360bd7e26087c3b4d7c6122ff51a9938e093c8bf
vinney cavallo <vcavallo@gmail.com>
2016-03-15 18:31:15 -0400

    Pass divide_wahts test

--- waht.rb
+++ waht.rb
@@ -2,4 +2,8 @@ class Waht
   def self.unwaht(wahtty_string)
     # i want this to split the wahtty_string into '....waht' chunks
   end
+
+  def self.divide_wahts(wahtty_string)
+    wahtty_string.split("waht")
+  end
 end
