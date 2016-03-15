2e8c50605d1014cb9bbefd95004cfd9fe7496865
vinney cavallo <vcavallo@gmail.com>
2016-03-15 18:46:25 -0400

    Get array instead of string....

--- waht.rb
+++ waht.rb
@@ -4,6 +4,9 @@ class Waht
     # 1. split the wahhty string into dot chunks
     # 2. for each dot chunk, convert it to ascii character
     # 3. join these all up in to one big string
+    divide_wahts(wahtty_string).collect do |dot_chunk|
+      dot_count_as_ascii(dot_chunk)
+    end
   end
 
   def self.divide_wahts(wahtty_string)
