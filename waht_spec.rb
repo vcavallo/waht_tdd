995e4f78694a9ce5b6d377667aea6c869947baf6
vinney cavallo <vcavallo@gmail.com>
2016-03-15 18:52:42 -0400

    fix feature test to include 'waht'

--- waht_spec.rb
+++ waht_spec.rb
@@ -3,17 +3,30 @@ require_relative 'waht'
 describe Waht do
 
   describe "features" do
+
     describe "unwahts a string of wahts" do
+      before :all do
+        pee = '.' * 112
+        @pee = pee + 'waht'
+      end
       context "with a single character" do
         it "converts it into a single character" do
-          pee = '.' * 112
-          expect(Waht.unwaht(pee)).to eq 'p'
+          expect(Waht.unwaht(@pee)).to eq 'p'
+        end
+      end
+      context "with multiple characters" do
+        it "converts it into a single string of many characters" do
+          eee = '.' * 101
+          eee = eee + 'waht'
+          expect(Waht.unwaht(eee + @pee)).to eq 'ep'
         end
       end
     end
+
   end
 
   describe "smaller pieces" do
+
     it "splits a wahtty string into separate pieces" do
       expect(Waht.divide_wahts('....waht....waht....waht').size).to eq 3
     end
@@ -27,6 +40,7 @@ describe Waht do
       thirty_three_dots = '.' * 33
       expect(Waht.dot_count_as_ascii(thirty_three_dots)).to eq "!"
     end
+
   end
 
 end
