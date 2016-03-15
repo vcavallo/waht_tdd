e59a18f63f28999954286b82d79d572ab5f80045
vinney cavallo <vcavallo@gmail.com>
2016-03-15 18:54:53 -0400

    Remove dev comment

--- waht.rb
+++ waht.rb
@@ -1,9 +1,5 @@
 class Waht
   def self.unwaht(wahtty_string)
-    # i'm ready to implement this now
-    # 1. split the wahhty string into dot chunks
-    # 2. for each dot chunk, convert it to ascii character
-    # 3. join these all up in to one big string
     divide_wahts(wahtty_string).collect do |dot_chunk|
       dot_count_as_ascii(dot_chunk)
     end.join
